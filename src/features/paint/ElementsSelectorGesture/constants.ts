6d542f3a7fdf49ea2997f86181ea45a64b8cf345
Andre-3rd-Viking <andre@t-b.ca>
2023-12-27 15:55:46 -0600

    feat: constants for selection movements

---
+++ src/features/paint/ElementsSelectorGesture/constants.ts
@@ -0,0 +1,11 @@
+export const CIRCLE_SIZE = 48;
+export const HALF_CIRCLE_SIZE = CIRCLE_SIZE / 2;
+
+export const SNAP_DELTA = 8;
+
+export enum SelectorMoveType {
+  NONE = 0,
+  BOUNDING_BOX = 1,
+  TOP_LEFT = 2,
+  BOTTOM_RIGHT = 3,
+}
