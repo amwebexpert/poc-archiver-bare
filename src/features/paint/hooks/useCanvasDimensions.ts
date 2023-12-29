d31ffb53451f16adaa463698b0cf21c7bd3ab0dc
Andre-3rd-Viking <andre@t-b.ca>
2023-12-29 08:58:19 -0600

    feat: useCanvasDimensions hook typing

---
+++ src/features/paint/hooks/useCanvasDimensions.ts
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+import { computeMaxDimensionsForAspectRatio_16_9 } from "../utils/canvasUtils";
+import { CanvasDimensions, CanvasSurface, DEFAULT_DIMENSIONS } from "../constants";
+
+export const useCanvasDimensions = () => {
+  const [canvasDimensions, setCanvasDimensions] = useState<CanvasDimensions>();
+  const isCanvasDimensionsAvailable = !!canvasDimensions;
+
+  const onCanvasParentLayoutDimensions = ({ width, height }: CanvasSurface) =>
+    setCanvasDimensions(computeMaxDimensionsForAspectRatio_16_9({ width, height }));
+
+  return {
+    isCanvasDimensionsAvailable,
+    canvasDimensions,
+    onCanvasParentLayoutDimensions,
+  };
+};
