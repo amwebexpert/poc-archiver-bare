836d5caae35a6d93a0b672284cc218c722ffaed2
Andre-3rd-Viking <andre@t-b.ca>
2023-12-29 16:10:34 -0600

    refactor: expandable toolbar cleanup

--- src/features/paint/components/ExpandableToolbar.tsx
+++ src/features/paint/components/ExpandableToolbar.tsx
@@ -1,11 +1,13 @@
 import { FunctionComponent, PropsWithChildren, useState } from "react";
 import { StyleSheet, View, ViewProps } from "react-native";
-import { IconButton } from "react-native-paper";
+import { IconButton, useTheme } from "react-native-paper";
 import Animated, { useAnimatedStyle, useSharedValue, withSpring, withTiming } from "react-native-reanimated";
+import { AppTheme } from "../../../theme";
 
 type ExpandableToolbarProps = PropsWithChildren<{ fullWidth?: number }> & ViewProps;
 
 export const ExpandableToolbar: FunctionComponent<ExpandableToolbarProps> = ({ children, fullWidth = 0, ...rest }) => {
+  const styles = useStyles();
   const [isExpanded, setIsExpanded] = useState(true);
   const expansion = useSharedValue(1);
   const animatedStyles = useAnimatedStyle(() => ({
@@ -32,16 +34,20 @@ export const ExpandableToolbar: FunctionComponent<ExpandableToolbarProps> = ({ c
   );
 };
 
-const styles = StyleSheet.create({
-  expandToggleButton: {
-    backgroundColor: "black",
-    borderRadius: 4,
-    height: "100%",
-    marginLeft: 8,
-    width: 24,
-  },
-  fixedToolbarContainer: {
-    flexDirection: "row",
-    overflow: "hidden",
-  },
-});
+const useStyles = () => {
+  const theme = useTheme() as AppTheme;
+
+  return StyleSheet.create({
+    expandToggleButton: {
+      backgroundColor: "black",
+      borderRadius: 4,
+      height: "100%",
+      marginLeft: theme.spacing(1),
+      width: 24,
+    },
+    fixedToolbarContainer: {
+      flexDirection: "row",
+      overflow: "hidden",
+    },
+  });
+};
