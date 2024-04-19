a4f7a87657c09f45e51de8a8ac3922747294d2e7
Andre-3rd-Viking <andre@t-b.ca>
2024-04-19 16:48:03 -0600

    chore: add reactotron-react-native to the project

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ import { startNetworkLogging } from "react-native-network-logger";
 import { name as appName } from "./app.json";
 import App from "./src/App";
 
+if (__DEV__) {
+  require("./src/utils/reactotron-config");
+}
+
 startNetworkLogging();
 
 AppRegistry.registerComponent(appName, () => App);
