42862d2b5d301eba5c5d1c996d482e776749840f
Andre-3rd-Viking <andre@t-b.ca>
2024-04-21 15:27:42 -0600

    feat: add default snackbar duration

--- src/components/snack-bar/SnackbarProvider.tsx
+++ src/components/snack-bar/SnackbarProvider.tsx
@@ -2,21 +2,28 @@ import { PropsWithChildren, createContext, useContext, useState } from "react";
 
 import { Snackbar } from "react-native-paper";
 
+const DEFAULT_DURATION = 2 * 1000;
+
 export const SnackbarContext = createContext({
-  showSnackbarMessage: (_: string) => {},
+  showSnackbarMessage: (_msg: string, _duration = DEFAULT_DURATION) => {},
 });
 
 export const useSnackbar = () => useContext(SnackbarContext);
 
 export const SnackbarProvider = ({ children }: PropsWithChildren): JSX.Element => {
   const [message, setMessage] = useState("");
-  const showSnackbarMessage = setMessage;
+  const [duration, setDuration] = useState(DEFAULT_DURATION);
+
+  const showSnackbarMessage = (msg: string, duration = DEFAULT_DURATION) => {
+    setMessage(msg);
+    setDuration(duration);
+  };
 
   return (
     <SnackbarContext.Provider value={{ showSnackbarMessage }}>
       {children}
 
-      <Snackbar visible={message.length > 0} onDismiss={() => setMessage("")} duration={Snackbar.DURATION_SHORT}>
+      <Snackbar visible={message.length > 0} onDismiss={() => setMessage("")} duration={duration}>
         {message}
       </Snackbar>
     </SnackbarContext.Provider>
